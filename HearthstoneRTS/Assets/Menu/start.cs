184bd4a9bf62e879743207f5cffde59cd21cbb2f
Andrew <colvinwandrew@gmail.com>
2015-08-31 16:54:54 -0500

    Options / Maps

--- HearthstoneRTS/Assets/Menu/start.cs
+++ HearthstoneRTS/Assets/Menu/start.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class start : MonoBehaviour {
 
+    public int world = 0;
+
     public void OnClick()
     {
-         Application.LoadLevel("IslandScene");
+        if (world == 0)
+            Application.LoadLevel("IslandScene");
+        else if (world == 1)
+            Application.LoadLevel("MatchScene");
     }
 }
