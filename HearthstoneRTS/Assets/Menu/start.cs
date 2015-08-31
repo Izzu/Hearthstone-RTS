0a9c24e799ece811e28f8137c3857636b1810e12
Andrew <colvinwandrew@gmail.com>
2015-08-31 16:28:29 -0500

    messing with events

---
+++ HearthstoneRTS/Assets/Menu/start.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+    public bool begin;
+
+    void Start()
+    {
+        if(begin)
+            Application.LoadLevel("IslandScene");
+    }
+}
