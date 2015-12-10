9641061e4256f337bf605a9a7b146eb27a0bc1bf
ipc0385 <ipc0385@louisiana.edu>
2015-12-09 19:39:09 -0600

    player resources

---
+++ HearthstoneRTS/Assets/Scripts/Components/Resource_View_Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Resource_View_Script : MonoBehaviour {
+
+	[SerializeField]
+	PlayerScript myPlayer;
+
+	[SerializeField]
+	Text mySupply, myGold, myMana;
+
+	// Update is called once per frame
+	void Update () {
+
+		mySupply.text = myPlayer.myDemand + " / " + myPlayer.mySupply;
+
+		myGold.text = myPlayer.myGold.ToString();
+		myMana.text = myPlayer.myMana + " / " + myPlayer.myManaCap;
+
+	}
+}
