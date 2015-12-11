a8e62c5af9016d21f940f11198bf1bef4336309a
ipc0385 <ipc0385@louisiana.edu>
2015-12-10 19:45:58 -0600

    With Panner Bounds

---
+++ HearthstoneRTS/Assets/Zoom_Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoom_Script : MonoBehaviour 
+{
+	
+	void Update()
+	{
+		var d = Input.GetAxis("Mouse ScrollWheel");
+		if (d > 0f)
+		{
+			// scroll up
+			transform.position = new Vector3(transform.position.x, transform.position.y * 0.95f, transform.position.z);
+		}
+		else if (d < 0f)
+		{
+			// scroll down
+			transform.position = new Vector3(transform.position.x, transform.position.y * 1.05f, transform.position.z);
+		}
+	}
+
+}
